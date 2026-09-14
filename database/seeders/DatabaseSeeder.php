2e9145579f19007b09e7909cc701af9a6833ae51
Arung Ikhsani Wijaya <100556943+ArungIW2@users.noreply.github.com>
2026-09-15 05:35:32 +0700

    feat(auth): add default super admin seeder

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::updateOrCreate(
+            ['email' => 'admin@caseer.test'],
+            [
+                'name' => 'Caseer Super Admin',
+                'password' => Hash::make('ChangeMe123!'),
+                'is_active' => true,
+                'role' => 'super_admin',
+                'store_id' => null,
+            ],
+        );
+    }
+}
