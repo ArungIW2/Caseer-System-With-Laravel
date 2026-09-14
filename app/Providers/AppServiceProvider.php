21f5871fe94c6ea20a172f36ee4e9b41a7f31d64
Arung Ikhsani Wijaya <100556943+ArungIW2@users.noreply.github.com>
2026-09-15 05:28:37 +0700

    chore: add application service provider

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        // Application bindings will be registered here.
+    }
+
+    public function boot(): void
+    {
+        // Application bootstrapping will be registered here.
+    }
+}
